7d10eb05924eae0e662f21b16ee26fbd185f90ec
Arun Wosti <arunwosti.naxa@gmail.com>
2023-07-30 12:50:58 -0400

    Added services page, gallery page and services component

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -5,11 +5,54 @@ function ServicesComponents() {
 	console.log('test');
   return (
     <div>
+        
         {/*<!-- services --> */} 
-		<div className='message-box'>
-            <span className='msg-text'><span className='congrats-txt'>Sorry!</span> Service page is under development.</span>
-        </div>
+<section class="services py-5" id="services">
+	<div class="container py-lg-5 py-3">
+		<div class="row service-grid-grids text-center">
+			<div class="col-lg-4 col-md-6 service-grid service-grid1">
+				<Service classname={'fa fa-puzzle-piece'} headingfour={'Skilled Barbers'}
+                para={'Perspiciatis unde omnis iste natus doloret ipsum volupte ut accusal ntium dolor remque laudantium, totam dolor.'}/>
+			</div>
+			<div class="col-lg-4 col-md-6 service-grid service-grid2 mt-md-0 mt-5">
+                <Service classname={'fa fa-scissors'} headingfour={'Hair stylists'}
+                    para={'Perspiciatis unde omnis iste natus doloret ipsum volupte ut accusal ntium dolor remque laudantium, totam dolor.'}/>
+				
+			</div>
+			
+			<div class="col-lg-4 col-md-6 service-grid service-grid3 mt-lg-0 mt-5">
+                <Service classname={'fa fa-sliders'} headingfour={'Beard Grooming'}
+                    para={'Perspiciatis unde omnis iste natus doloret ipsum volupte ut accusal ntium dolor remque laudantium, totam dolor.'}/>
+			</div>
+		</div>
+		<div class="row mt-5">
+			<div class="col-md-6 p-md-0">
+				<div class="bg-image-left">	
+					<h4>skilled barbers</h4>
+				</div>
+			</div>
+			<div class="col-md-6 p-md-0">
+				<div class="bg-image-right">
+					<h4>skilled barbers</h4>
+				</div>
+				<div class="row">
+					<div class="col-md-6 pr-md-0">
+						<div class="bg-image-bottom1">
+							<h4>Trimming</h4>
+						</div>
+					</div>
+					<div class="col-md-6 pl-md-0">
+						<div class="bg-image-bottom2">
+							<h4>Shaving</h4>
+						</div>
+					</div>
+				</div>	
+			</div>	
+		</div>		
+	</div>		
+</section>
 {/*<!-- //services --> */} 
+
     </div>
   )
 }
--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -8,11 +8,115 @@ function Gallery() {
        <BannerComponents/>
 
        <PagedetailComponent path={'/gallery'} page={'  Gallery'}/>
+
 {/* <!-- gallery --> */}
-<div className='message-box'>
-            <span className='msg-text'><span className='congrats-txt'>Sorry!</span> Gallery page is under development.</span>
-        </div>
+<div class="gallery py-sm-5 py-4 position-relative" id="gallery">
+	<div class="container py-lg-3">
+		<h2 class="heading text-center mb-lg-5 mb-sm-4 mb-3 editContent">Our Gallery</h2>
+		<div class="news-grids gal">
+			<div class="row">
+				<div class="col-lg-3 col-md-4 col-sm-6 gal-img">
+					<a href="#gal1"><img src="assets/images/g1.jpg" alt="projects image" class="img-fluid"/></a>
+					<h5>Hairstyle 1</h5>
+					<p>Nullam dui mi, vulputater act metus dolor sit amet.</p>
+				</div>
+				<div class="col-lg-3 col-md-4 col-sm-6 gal-img my-md-0">
+					<a href="#gal2"><img src="assets/images/g2.jpg" alt="projects image" class="img-fluid"/></a>
+					<h5>Hairstyle 2</h5>
+					<p>Nullam dui mi, vulputater act metus dolor sit amet.</p>
+				</div>
+				<div class="col-lg-3 col-md-4 col-sm-6 gal-img">
+					<a href="#gal3"><img src="assets/images/g3.jpg" alt="projects image" class="img-fluid"/></a>
+					<h5>Hairstyle 3</h5>
+					<p>Nullam dui mi, vulputater act metus dolor sit amet.</p>
+				</div>
+				<div class="col-lg-3 col-md-4 col-sm-6 gal-img">
+					<a href="#gal4"><img src="assets/images/g4.jpg" alt="projects image" class="img-fluid"/></a>
+					<h5>Hairstyle 4</h5>
+					<p>Nullam dui mi, vulputater act metus dolor sit amet.</p>
+				</div>
+				<div class="col-lg-3 col-md-4 col-sm-6 gal-img">
+					<a href="#gal5"><img src="assets/images/g5.jpg" alt="projects image" class="img-fluid"/></a>
+					<h5>Hairstyle 5</h5>
+					<p>Nullam dui mi, vulputater act metus dolor sit amet.</p>
+				</div>
+				<div class="col-lg-3 col-md-4 col-sm-6 gal-img my-md-0">
+					<a href="#gal6"><img src="assets/images/g6.jpg" alt="projects image" class="img-fluid"/></a>
+					<h5>Hairstyle 6</h5>
+					<p>Nullam dui mi, vulputater act metus dolor sit amet.</p>
+				</div>
+				<div class="col-lg-3 col-md-4 col-sm-6 gal-img">
+					<a href="#gal1"><img src="assets/images/g1.jpg" alt="projects image" class="img-fluid"/></a>
+					<h5>Hairstyle 7</h5>
+					<p>Nullam dui mi, vulputater act metus dolor sit amet.</p>
+				</div>
+				<div class="col-lg-3 col-md-4 col-sm-6 gal-img">
+					<a href="#gal2"><img src="assets/images/g2.jpg" alt="projects image" class="img-fluid"/></a>
+					<h5>Hairstyle 8</h5>
+					<p>Nullam dui mi, vulputater act metus dolor sit amet.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	{/* <!-- gallery popups --> */}
+	{/* <!-- popup--> */}
+	<div id="gal1" class="popup-effect">
+		<div class="popup">
+			<img src="images/g1.jpg" alt="Popup Image" class="img-fluid" />
+			<p>Nullam dui mi, vulputater act metus dolor sit amet, onec consequat sapien utleo dolor rhoncus.</p>
+			<a class="close" href="#gallery">&times;</a>
+		</div>
+	</div>
+	{/* <!-- //popup --> */}
+	{/* <!-- popup--> */}
+	<div id="gal2" class="popup-effect">
+		<div class="popup">
+			<img src="images/g2.jpg" alt="Popup Image" class="img-fluid" />
+			<p>Nullam dui mi, vulputater act metus dolor sit amet, onec consequat sapien utleo dolor rhoncus.</p>
+			<a class="close" href="#gallery">&times;</a>
+		</div>
+	</div>
+	{/* <!-- //popup --> */}
+	{/* <!-- popup--> */}
+	<div id="gal3" class="popup-effect">
+		<div class="popup">
+			<img src="images/g3.jpg" alt="Popup Image" class="img-fluid" />
+			<p>Nullam dui mi, vulputater act metus dolor sit amet, onec consequat sapien utleo dolor rhoncus.</p>
+			<a class="close" href="#gallery">&times;</a>
+		</div>
+	</div>
+	{/* <!-- //popup --> */}
+	{/* <!-- popup--> */}
+	<div id="gal4" class="popup-effect">
+		<div class="popup">
+			<img src="images/g4.jpg" alt="Popup Image" class="img-fluid" />
+			<p>Nullam dui mi, vulputater act metus dolor sit amet, onec consequat sapien utleo dolor rhoncus.</p>
+			<a class="close" href="#gallery">&times;</a>
+		</div>
+	</div>
+	{/* <!-- //popup --> */}
+	{/* <!-- popup--> */}
+	<div id="gal5" class="popup-effect">
+		<div class="popup">
+			<img src="images/g5.jpg" alt="Popup Image" class="img-fluid" />
+			<p>Nullam dui mi, vulputater act metus dolor sit amet, onec consequat sapien utleo dolor rhoncus.</p>
+			<a class="close" href="#gallery">&times;</a>
+		</div>
+	</div>
+	{/* <!-- //popup --> */}
+	{/* <!-- popup--> */}
+	<div id="gal6" class="popup-effect">
+		<div class="popup">
+			<img src="images/g6.jpg" alt="Popup Image" class="img-fluid" />
+			<p>Nullam dui mi, vulputater act metus dolor sit amet, onec consequat sapien utleo dolor rhoncus.</p>
+			<a class="close" href="#gallery">&times;</a>
+		</div>
+	</div>
+	{/* <!-- //popup --> */}
+	{/* <!-- //gallery popups --> */}
+</div>
 {/* <!-- //gallery --> */}
+
 
 
     </div>
